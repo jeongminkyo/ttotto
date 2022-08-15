f2c5bd2491582e67a14605ec30e6bad5a0226d40
Jeong Minkyo <redwonder@naver.com>
2022-08-15 23:21:16 +0900

    feat : rest docs 설정 추가

--- src/test/java/com/landvibe/ttotto/TtottoApplicationTests.java
+++ src/test/java/com/landvibe/ttotto/TtottoApplicationTests.java
@@ -2,9 +2,11 @@ package com.landvibe.ttotto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class TtangtotoApplicationTests {
+@ActiveProfiles("test")
+class TtottoApplicationTests {
 
 	@Test
 	void contextLoads() {
